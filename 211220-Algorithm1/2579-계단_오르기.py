8956d5788f7c72ff5244de8dbc69b985550a18e0
goo314 <rn0731@naver.com>
2021-12-22 17:42:26 +0900

    Update 2579-계단_오르기.py

--- 211220-Algorithm1/2579-계단_오르기.py
+++ 211220-Algorithm1/2579-계단_오르기.py
@@ -1,4 +1,23 @@
 # Dynamic Programming
+'''
+Dynamic programming is...
+simplifying a complicated problem
+by breaking it down into simpler sub-problems in recursive manner
+
+ex)
+def fibo(n):
+    if n == 0 or n == 1:
+        return 1
+    else return fibo(n-1) + fibo(n-2)
+
+-> store in list
+
+fibos = [1, 1]
+def fibo(n):
+    if n not in fibos:
+        fibos.append(fibos[n-1]+fibos[n-2])
+    return fibos[n]
+'''
 n = int(input())
 stairs = [int(input()) for _ in range(n)]
 
