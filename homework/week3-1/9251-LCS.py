877039231ad719fa65085d85a08034df3ba7f3db
goo314 <rn0731@naver.com>
2021-12-27 17:44:59 +0900

    Create: solve #9251

---
+++ homework/week3-1/9251-LCS.py
@@ -0,0 +1,16 @@
+s1 = input()
+s2 = input()
+
+n, m = len(s1), len(s2)
+dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+for i in range(1,m+1):
+    for j in range(1,n+1):
+        if s2[i-1] == s1[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[m][n])
+
+# O(nm), space: nm
